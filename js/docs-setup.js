2bf9ffeaf78421f175627f7188ccb42e547ccbdb
Andrew Stuart <andrew.stuart2@gmail.com>
2017-08-22 09:06:08 -0700

    Update 2017-08-22T16:06:07.193Z

---
+++ js/docs-setup.js
@@ -0,0 +1,50 @@
+NG_DOCS={
+  "sections": {
+    "api": "ng-a11y library"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "ng-arc.directive:withService",
+      "shortName": "withService",
+      "type": "directive",
+      "moduleName": "ng-arc",
+      "shortDescription": "Injects a service into a new scope the DOM element.",
+      "keywords": "aliased angular api bar baz case directive dom element es es2 example exampleservice exampleservicetwo exposed expression foo function html inject injects js module multiple ng-arc scope service services with-service withservice withserviceexample"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/dist/ng-arc.js"
+    ],
+    "styles": [],
+    "title": "ng-a11y library",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js",
+    "js/dist/ng-arc.js"
+  ]
+};
